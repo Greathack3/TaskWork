35e58d0eb5fa0dba1e02024920039ac9e5075b2c
Greathack3 <80649594+Greathack3@users.noreply.github.com>
2025-06-30 01:52:07 +0200

    Initial commit

---
+++ resources/js/Components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper" :style="{ backgroundColor: backgroundColor }">
+    <button @click="changeColor">Change Background</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      backgroundColor: 'white',
+    };
+  },
+  methods: {
+    changeColor() {
+      const colors = ['#FF6B6B', '#4ECDC4', '#FFD93D', '#6A4C93', '#1A535C'];
+      this.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.5s;
+}
+
+button {
+  padding: 1rem 2rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+  border: none;
+  border-radius: 8px;
+  background-color: #333;
+  color: white;
+  cursor: pointer;
+}
+</style>
+
+
+
---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import HelloWorld from './Components/HelloWorld.vue';
+
+const app = createApp({});
+app.component('hello-world', HelloWorld);
+app.mount('#app');
+
+
+
